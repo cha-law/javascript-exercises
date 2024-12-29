c87ec08ded419fb542c77dec5b073589cc2347b0
cha-law <char_law@outlook.com>
2024-12-29 11:15:13 +0000

    Completed Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringRepeated, numRepeated) {
+    let message = "";
 
+    if (Math.sign(numRepeated) === -1) {
+        return "ERROR"
+    }
+    
+    for (i = 1; i < numRepeated+1; i++) {
+        message = message.concat(stringRepeated)
+    }
+    return message;
 };
 
 // Do not edit below this line
