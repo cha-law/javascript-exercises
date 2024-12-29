0c3c556e137d47c0feb31ec1d4e8ef75d6698e21
cha-law <char_law@outlook.com>
2024-12-29 12:21:17 +0000

    1 test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
 
+    for (i = firstNum; i <= lastNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
