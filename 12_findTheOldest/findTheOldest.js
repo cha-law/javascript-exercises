3daecac4dc3f9cdc37f9bcde3b911a47cffb792e
cha-law <char_law@outlook.com>
2025-01-08 19:55:26 +0000

    Exercise 12 Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = [0, null];
+    for (i = 0; i < people.length; i++) {
+        if ("yearOfDeath" in people[i]) {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            currentAge = 2024 - people[i].yearOfBirth
+        }
+        
+        if (currentAge > oldestAge[0]) {
+            oldestAge = [currentAge, i];
+        }
+    }
+    
+    // Get the person
+    let oldest = people[oldestAge[1]]
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
