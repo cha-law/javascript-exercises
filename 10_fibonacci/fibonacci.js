2f9afd8eb88c552ad9816b1695906fa83d7474b2
cha-law <char_law@outlook.com>
2025-01-08 19:20:01 +0000

    Exercise 10 Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(amount) {
+    if (amount < 0) return "OOPS";
+    let previousNum = 0; 
+    let currentNum = 1;
+    for (let i = amount; i > 0; i--) {
+        newNum = previousNum + currentNum;
+        previousNum = currentNum;
+        currentNum = newNum; 
+    }
+    return previousNum;
 };
 
 // Do not edit below this line
