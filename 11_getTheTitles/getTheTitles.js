6b0b8d9d443f8587a011652aac234c7b8f993517
cha-law <char_law@outlook.com>
2025-01-08 19:31:38 +0000

    Exercise 11 Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    returnArray = [];
+    array.forEach(function(object) {
+        returnArray.push(object.title)
+    });
+    return returnArray;
 };
 
 // Do not edit below this line
