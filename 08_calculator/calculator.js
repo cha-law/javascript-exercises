de5a4157f8edd1a68a74e5676b07d9d00d33b73a
cha-law <char_law@outlook.com>
2025-01-08 18:46:21 +0000

    Exercise 08 Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array.reduce((total, currentItem) => {
+      return total + currentItem;
+    });
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, currentItem) => {
+    return acc * currentItem;
+  });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  for (let i = num; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
